afcd4d17c5e3ebee76a5768dea5e93708f7d71e9
rock <rock@rabian-rock-pro>
2021-02-10 18:27:40 +0800

    2021年02月10日 18点27分13秒 同步更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,17 +1,9 @@
 
-
-
-
 # 半点红包雨
 30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js >> /scripts/logs/jd_live_redrain_half.log 2>&1
 
 # 官方号直播红包雨
 0 0,9,11,13,15,17,19,20,21,22,23 * * * node /scripts/jd_live_redrain_offical.js >> /scripts/logs/jd_live_redrain_offical.log 2>&1
-
-
-
-
-
 
 
 
@@ -22,9 +14,13 @@
 # 京东赚京东开团
 10 7-23 1-31 1 * node /scripts/jd_zjdtuan.js >> /scripts/logs/jd_zjbtuan.log 2>&1
 
-
-# 5G狂城
-10 7-23/4 * * * node /scripts/jd_5g.js >> /scripts/logs/jd_5g.log 2>&1
-
 # 泡泡大战
 33 7 * * * node /scripts/jd_paopao.js >> /scripts/logs/jd_paopao.log 2>&1
+
+
+
+# 国际盲盒
+5 0 10-15 2 *  node /scripts/jd_gjmh.js >> /scripts/logs/jd_gjmh.log 2>&1
+
+#直播红包雨
+0,50 0,9,11,13,15,17,19,20,21,23 8-18 2 *  node /scripts/red_rain.js  >> /scripts/logs/red_rain.log 2>&1
