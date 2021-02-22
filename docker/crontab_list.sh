db9e41487d011f02757df10dec93b7fcd457e7b2
rock <rock@rabian-rock-pro>
2021-02-23 01:29:56 +0800

    2021年02月23日 01点29分35秒 同步更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -15,8 +15,10 @@
 
 
 #粉丝互动
-3 10 * * *  node /scripts/jd_fanslove.js >> /scripts/logs/jd_fanslove.js 2>&1
+3 10 * * *  node /scripts/jd_fanslove.js >> /scripts/logs/jd_fanslove.log 2>&1
 #美丽美容院
-0 0,9,13,20 * * *  node /scripts/jd_mlyjy.js >> /scripts/logs/jd_mlyjy.js 2>&1
+0 0,9,13,20 * * *  node /scripts/jd_mlyjy.js >> /scripts/logs/jd_mlyjy.log 2>&1
 #百变大咖秀
-10 10,11 * * 2-5  node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.js 2>&1
+10 10,11 * * 2-5  node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
+#华硕-iqiyi
+0 0 22-28 2 *  node /scripts/jd_asus_iqiyi.js >> /scripts/logs/jd_asus_iqiyi.log 2>&1
